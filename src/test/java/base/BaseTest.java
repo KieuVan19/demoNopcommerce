3541ba757a4292ce29889837d2b717fb3f6f9c4f
VanHa <hakieuvan96@gmail.com>
2025-10-08 20:54:13 +0700

    fix chromeDriver

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -3,8 +3,12 @@ package base;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BaseTest {
 
@@ -13,7 +17,28 @@ public class BaseTest {
     @BeforeMethod
     public void setUp() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        List<String> args = new ArrayList<>();
+
+        // Common args (safe for all environments)
+        args.add("--no-sandbox");
+        args.add("--disable-dev-shm-usage");
+        args.add("--disable-gpu");
+        args.add("--remote-allow-origins=*");
+        args.add("--window-size=1920,1080");
+        args.add("--disable-notifications");
+        args.add("--disable-extensions");
+
+        // unique user data dir to avoid "already in use" error
+        args.add("--user-data-dir=" + System.getProperty("java.io.tmpdir") + "/chrome-" + System.currentTimeMillis());
+
+        // Run headless only in CI (so your local dev still shows browser)
+        if (System.getenv("CI") != null) {
+            args.add("--headless=new"); // new headless mode for Chrome 109+
+        }
+
+        options.addArguments(args);
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
     }
 
